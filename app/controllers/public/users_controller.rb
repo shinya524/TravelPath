12e36cdf86b6ec7322411b99af4f92f85974149c
shinya524 <gheena1800@gmail.com>
2022-10-03 08:50:47 +0000

    add プロフィール編集機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -8,6 +8,13 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
