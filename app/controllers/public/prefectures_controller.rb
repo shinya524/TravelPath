01d9e161643bab29ef220f424f52c5f4ee81a52e
shinya524 <gheena1800@gmail.com>
2022-10-13 10:48:32 +0000

    add ジャンル機能

--- app/controllers/public/prefectures_controller.rb
+++ app/controllers/public/prefectures_controller.rb
@@ -1,7 +1,11 @@
 class Public::PrefecturesController < ApplicationController
   def show
     @prefecture = Prefecture.find(params[:id])
-    @prefecture_post_spots = PostSpot.where(prefecture_id: @prefecture.id).page(params[(:page)])
+    prefecture_post_spots = PostSpot.where(prefecture_id: @prefecture.id)
+    if params[:genre_id]
+      prefecture_post_spots = prefecture_post_spots.where(genre_id: params[:genre_id])
+    end
+    @prefecture_post_spots = prefecture_post_spots.page(params[:page])
     @genres = Genre.all
   end
 
