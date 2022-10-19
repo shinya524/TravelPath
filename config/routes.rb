6f29bf3876252b25f5251c55c408713876506a6a
shinya524 <gheena1800@gmail.com>
2022-10-19 08:53:28 +0000

    add ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
       sessions: 'public/sessions'
     }
 
+    devise_scope :user do
+      post 'users/guest_sign_in', to: 'sessions#guest_sign_in'
+    end
+
     root to: "homes#top"
     get "/about" => "homes#about"
 
